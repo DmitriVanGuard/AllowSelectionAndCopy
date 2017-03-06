988d80b01ff14a89d8e65aacb3bc976ddc193502
DmitriVanGuard <dimavanguard@gmail.com>
2017-03-06 14:41:19 +0200

    New function to pass element, attribute, and code to run. Mouse selection block free

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+var imageElement = document.createElement('img');
 setTimeout(allowSelect, 2000);
 
 function allowSelect(){
-	var scr = document.createElement('img');
-	scr.src = "allowSelectionAndCopy.jpg";
-	scr.setAttribute("onerror", "document.ondragstart=null; document.onselectstart=null; document.oncontextmenu=null; document.body.setAttribute('oncopy', ''); window.onkeydown = null; window.onkeyup = null; window.onkeypress = null;");
-	document.body.setAttribute('style', 'user-select: text !important');
-	document.body.appendChild(scr);
-	document.body.removeChild(scr);
+	imageElement.src = "allowSelectionAndCopy.jpg";
+
+	//First argument - tag(element name), second argument - tag attribute, following arguements are code that you want to run
+	setElementAttr(imageElement, "onerror",
+		"document.ondragstart = document.onselectstart = document.oncontextmenu = null;",
+		"document.body.setAttribute('oncopy', '');",
+		"document.body.setAttribute('style', 'user-select: text !important');",
+		"window.onkeydown = window.onkeyup = window.onkeypress = null;",
+		"window.onmousemove = window.onmousedown = window.onmouseup = function(e){window.getSelection().removeAllRanges = function(){};};"
+		);
+	
+	document.body.appendChild(imageElement);
+	document.body.removeChild(imageElement);
+}
+
+function setElementAttr(element){
+	var attribute = {}
+	attribute.name = arguments[1];
+	attribute.codeToRun = [];
+	
+	for(var i = 2; i < arguments.length; i++){
+		attribute.codeToRun[i-2] = arguments[i];
+	}
+
+	attribute.codeToRun = attribute.codeToRun.join('');
+	element.setAttribute(attribute.name, attribute.codeToRun);
 }
