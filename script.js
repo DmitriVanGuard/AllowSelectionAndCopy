7c8feac0074020b3068dbc5667d9f12bca75d3be
DmitriVanGuard <dimavanguard@gmail.com>
2017-03-06 12:10:36 +0200

    Ctrl+C block free

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+setTimeout(allowSelect, 2000);
 
-	setTimeout(allowSelect, 2000);
-
-	function allowSelect(){
-		var scr = document.createElement('img');
-		scr.src = "allowSelectionAndCopy.jpg";
-		scr.setAttribute("onerror", "document.ondragstart=null; document.onselectstart=null; document.oncontextmenu=null; document.body.setAttribute('oncopy', '');");
-		document.body.setAttribute('style', 'user-select: text !important');
-		document.body.appendChild(scr);
-		document.body.removeChild(scr);
-	}
+function allowSelect(){
+	var scr = document.createElement('img');
+	scr.src = "allowSelectionAndCopy.jpg";
+	scr.setAttribute("onerror", "document.ondragstart=null; document.onselectstart=null; document.oncontextmenu=null; document.body.setAttribute('oncopy', ''); window.onkeydown = null; window.onkeyup = null; window.onkeypress = null;");
+	document.body.setAttribute('style', 'user-select: text !important');
+	document.body.appendChild(scr);
+	document.body.removeChild(scr);
+}
