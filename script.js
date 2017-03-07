43c2922e604e9bfb082413c107c4d0acb290c419
DmitriVanGuard <dimavanguard@gmail.com>
2017-03-07 17:14:00 +0200

    key combination logic

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var imageElement = document.createElement('img');
 var newStyleTag = document.createElement('style');
 setTimeout(allowSelect, 2000);
+var ultraMode = {
+	toggle: false,
+	17: false, // ctrl
+	16: false, // shift
+	65: false // a
+};
+// var parentModification = function(event){console.log("childing");var child = event.target || event.srcElement; child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null; while(child.parentNode){ child = child.parentNode; child.onmousedown = child.oncontextmenu = child.ondragstart = 	child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null;}};
+//Inherited Copy Unblock Logic
 
 function allowSelect(){
 	//Last argument must be a style that you want to set!
@@ -9,12 +17,23 @@ function allowSelect(){
 	//First argument - tag(element name), second argument - tag attribute, following arguements are code that you want to run
 	setElementJS(imageElement, "onerror",
 		"window.onkeydown = window.onkeyup = window.onkeypress = null;",
-		"(function(){var parentModification = function(event){var child = event.target || event.srcElement; child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null; while(child.parentNode){ child = child.parentNode; child.onmousedown = child.oncontextmenu = child.ondragstart = 	child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null;}};document.addEventListener('selectstart', parentModification, true);})();"
+		"(function(global){var parentModification = function(event){console.log('childing');var child = event.target || event.srcElement; child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null; while(child.parentNode){ child = child.parentNode; child.onmousedown = child.oncontextmenu = child.ondragstart = 	child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null;}};global.parentModification = parentModification;})(window);"
 	);
 	window.addEventListener('mousedown', function(e){ e.stopPropagation(); }, true);
-	document.head.appendChild(newStyleTag);
+
 	document.body.appendChild(imageElement);
+
+	var ultraModeLogic = function(event){
+		keyPressed();
+		combinationPressed();
+	};
+
+	window.addEventListener('keydown', ultraModeLogic, true);
+	window.addEventListener('keyup', ultraModeLogic, true);
+	
+	document.head.appendChild(newStyleTag);
 	document.body.removeChild(imageElement);
+	
 }
 function setElementJS(element){
 	var attribute = {};
@@ -39,4 +58,29 @@ function setNewStyleTag(){
 	tags = tags.join(", ");
 	resultCss = tags + cssStyle;
 	newStyleTag.appendChild(document.createTextNode(resultCss));
+}
+function keyPressed(){
+	if(event.type == 'keydown'){
+		if(ultraMode.hasOwnProperty(event.keyCode)){
+			ultraMode[event.keyCode] = true;
+		}
+	}
+	else if(event.type == 'keyup'){
+		if(ultraMode.hasOwnProperty(event.keyCode)){
+			ultraMode[event.keyCode] = false;
+		}
+	}
+}
+
+function combinationPressed(){
+	if(ultraMode[17] && ultraMode[16] && ultraMode[65]){
+		if(ultraMode.toggle){
+			document.removeEventListener('selectstart', window.parentModification, true);
+			ultraMode.toggle = false;
+		}else{
+			document.addEventListener('selectstart', window.parentModification, true);
+			ultraMode.toggle = true;
+		}
+		console.log(window.parentModification);
+	}
 }
