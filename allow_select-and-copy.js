3ebdb49201eca71f4e2ab8147042d751d4d055cc
DmitriVanGuard <dimavanguard@gmail.com>
2017-03-13 14:23:27 +0200

    ::selection bypass

--- allow_select-and-copy.js
+++ allow_select-and-copy.js
@@ -1,28 +1,26 @@
-var ultraModeLogic = function (event){
-	ultraKeyPressed(event);
-	ultraCombinationPressed();
-}; 
-
-autoAllowSelectAndCopy(window, document, document.body);
-window.addEventListener('keydown', ultraModeLogic, true);
-window.addEventListener('keyup', ultraModeLogic, true);
-
-//Inherited copy unblock logic
-var parentModification = function(event){
-	var child = event.target || event.srcElement;
-	child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null;
-	while(child.parentNode){ 
-		child = child.parentNode;
-		child.addEventListener("selectstart", ultraPropagation, true);
-		child.addEventListener("mousedown", ultraPropagation, true);
-		child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = null;
-	}
-};
 var ultraMode = {
 	toggle: false,
 	18: false, 
 	16: false, 
 	65: false
+};
+var ultraModeLogic = function (event){
+	ultraKeyPressed(event);
+	ultraCombinationPressed();
+}; 
+autoAllowSelectAndCopy(window, document, document.body);
+window.addEventListener('keydown', ultraModeLogic, true);
+window.addEventListener('keyup', ultraModeLogic, true);
+window.addEventListener('copy', function(e){e.stopPropagation();}, true);
+
+//Inherited copy unblock logic
+var parentModification = function(event){
+	var child = event.target || event.srcElement;
+	child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = ultraPropagation;
+	while(child.parentNode){ 
+		child = child.parentNode;
+		child.onmousedown = child.oncontextmenu = child.ondragstart = child.onclick = child.onselectstart = child.onmousemove = child.oncopy = ultraPropagation;
+	}
 };
 var ultraPropagation = function(event){
 	if(ultraMode.toggle){
