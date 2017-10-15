ac7436722d8315e79a8964eb8dae2a726699d9a2
DmitriVanGuard <dimavanguard@gmail.com>
2017-10-15 17:11:15 +0300

    All logic inside script.js. Started working on popup.

--- script.js
+++ script.js
@@ -1,102 +1,142 @@
+var ultraMode = {
+	toggle: false,
+	18: false,
+	16: false,
+	65: false
+};
+
+var defaultEventHandlersContainer = [];
+
+var defaultHandlers = [
+	'onselectstart',
+	'oncopy',
+	'oncontextmenu',
+	'onclick',
+	'onkeypress',
+	'onkeyup',
+	'onkeydown',
+	'onmousedown',
+	'onmousemove',
+	'onmouseup'
+];
+
 chrome.runtime.sendMessage('wait');
 var newStyles = [];
-window.onload = allowSelect;
-function allowSelect(){
-	console.log("Loading extension");
+window.addEventListener('load', allowSelect);
+function allowSelect() {
+	console.log('Loading extension');
 	// console.log(document.readyState);
 	//Last argument must be style that you want to set. Optional argument (true, false) to create pseudo element ::selection
-	setNewStyles("body", "div", "a", "p", "span", "user-select: text !important;");
-	setNewStyles("p", "h1", "h2", "h3", "h4", "h5", "h6", "cursor: auto; user-select: text !important;");
-	setNewStyles("::selection", "background-color: #338FFF !important; color: #fff !important;");
+	setNewStyles('body', 'div', 'a', 'p', 'span', 'user-select: text !important;');
+	setNewStyles('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'cursor: auto; user-select: text !important;');
+	setNewStyles('::selection', 'background-color: #338FFF !important; color: #fff !important;');
 	setNewStyleTag(newStyles);
-	//Pass name of the script to load
-	setNewScriptTag("allow_select-and-copy.js");
+	autoAllowSelectAndCopy(defaultEventHandlersContainer, window, document, document.documentElement, document.body);
 	extensionReady();
 }
 
-function setNewStyles(){
+function setNewStyles() {
 	var lastArgumentIndex = arguments.length - 1,
-		cssStyle = "{" + arguments[lastArgumentIndex] + "}",
+		cssStyle = '{' + arguments[lastArgumentIndex] + '}',
 		tags = [],
 		resultCss;
-	for(var i = 0; i < lastArgumentIndex; i++){
+	for (var i = 0; i < lastArgumentIndex; i++) {
 		tags.push(arguments[i]);
 	}
-	tags = tags.join(", ");
+	tags = tags.join(', ');
 	resultCss = tags + cssStyle;
 	newStyles.push(resultCss);
 }
-function setNewStyleTag(stylesArray){
+function setNewStyleTag(stylesArray) {
 	var newStyleTag = document.createElement('style');
-	newStyleTag.type = "text/css";
-	for(var i = 0; i < stylesArray.length ; i++){
+	newStyleTag.type = 'text/css';
+	for (var i = 0; i < stylesArray.length; i++) {
 		newStyleTag.appendChild(document.createTextNode(stylesArray[i]));
 	}
 	document.head.appendChild(newStyleTag);
 	appendIFrame('head', newStyleTag);
 }
-function setNewScriptTag(scriptName){
-	var newScriptTag = document.createElement('script');
-	newScriptTag.type = 'text/javascript';
-	newScriptTag.src = chrome.extension.getURL(scriptName);
-	document.body.appendChild(newScriptTag); // Load script to page to see full potential
-	appendIFrame('body', newScriptTag);
-}
 
-function appendIFrame(target, obj){
+function appendIFrame(target, obj) {
 	iframes = window.frames;
-	for(var i = 0; i < iframes.length; i++){
-		try{
+	for (var i = 0; i < iframes.length; i++) {
+		try {
 			iframes[i].document[target].appendChild(obj);
-		}
-		catch(err){
-			console.log(err)
+			console.log('Appended Iframe');
+		} catch (err) {
+			// console.log(err);
 		}
 	}
-	console.log("Appended Iframe");
 }
 
-var ultraMode = {
-	toggle: false, 
-	18: false, 
-	16: false, 
-	65: false
-};
-function ultraKeyPressed(event){
-	if(event.type == 'keydown'){
-		if(ultraMode.hasOwnProperty(event.keyCode)){
+function ultraKeyPressed(event) {
+	if (event.type == 'keydown') {
+		if (ultraMode.hasOwnProperty(event.keyCode)) {
 			ultraMode[event.keyCode] = true;
 		}
-	}else if(event.type == 'keyup'){
-		if(ultraMode.hasOwnProperty(event.keyCode)){
+	} else if (event.type == 'keyup') {
+		if (ultraMode.hasOwnProperty(event.keyCode)) {
 			ultraMode[event.keyCode] = false;
 		}
 	}
 }
-function ultraCombinationPressed(){
-	if(ultraMode[18] && ultraMode[16] && ultraMode[65]){
-		if(ultraMode.toggle){
-			extensionReady();
-			console.log('ultra', false);
-		}
-		else{
-			ultraModeToggleChange(true);
-			console.log('ultra', true);
-		}
+function ultraCombinationPressed() {
+	if (ultraMode[18] && ultraMode[16] && ultraMode[65]) {
+		ultraMode.toggle = !ultraMode.toggle;
+		console.log('ultra', ultraMode.toggle);
+		toggleUltraHandlers('selectstart mousedown contextmenu copy', ultraPropagation, ultraMode.toggle);
 	}
 }
-var ultraModeLogic = function (event){
+var ultraModeLogic = function(event) {
 	ultraKeyPressed(event);
 	ultraCombinationPressed();
-}; 
+};
+
+function extensionReady() {
+	chrome.runtime.sendMessage('ready');
+}
+
+var ultraPropagation = function(event) {
+	if (ultraMode.toggle) {
+		event.stopPropagation();
+	}
+};
+
+// Saving default handlers to backup them if user disable extension in POPUP
+function autoAllowSelectAndCopy(obj, ...elems) {
+	elems.forEach((elem, index) => {
+		if (obj[index]) return;
+		var elemContainer = {};
+		elemContainer.refElem = elem;
+		defaultHandlers.forEach(function(item) {
+			elemContainer[item] = elem[item];
+			elem[item] = null;
+		});
+		obj.push(elemContainer);
+	});
+	console.log(obj);
+}
+
+function toggleUltraHandlers(events, callback, activate) {
+	events = events.split(' ');
+	if (activate) {
+		events.forEach(function(item) {
+			window.addEventListener(item, callback, true);
+		});
+		chrome.runtime.sendMessage('ultra');
+	} else {
+		events.forEach(function(item) {
+			window.removeEventListener(item, callback, true);
+		});
+		chrome.runtime.sendMessage('ready');
+	}
+}
 
 window.addEventListener('keydown', ultraModeLogic, true);
 window.addEventListener('keyup', ultraModeLogic, true);
 
-function ultraModeToggleChange(value){
-	ultraMode.toggle = value;
-	chrome.runtime.sendMessage(ultraMode.toggle);
-}
-function extensionReady(){
-	chrome.runtime.sendMessage('ready');
-}
+//Manage extension from a popup settings
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+	if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+});
